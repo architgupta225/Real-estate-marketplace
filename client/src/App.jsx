919553b7052df408bbc91d01b483be51f1d18d65
Architgupta225 <architgupta225@gmail.com>
2024-02-21 19:44:09 +0530

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import About from './pages/About'
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-out" element={<SignOut />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/about" element={<About />} />
+
+
+    </Routes>
+    </BrowserRouter>
   )
 }
