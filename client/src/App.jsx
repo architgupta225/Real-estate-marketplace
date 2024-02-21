95eb1e8eb8f13743be61f9266e7dcb8ad2faf87c
architgupta225 <architgupta225@gmail.com>
2024-02-21 15:28:29 +0530

    create page and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Home from './pages/Home'
+import SignOut from './pages/SignOut'
+import SingIn from './pages/SingIn'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 export default function App() {
   return (
-    <div>App - Archit</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SingIn />} />
+        <Route path='/sign-out' element={<SignOut />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
