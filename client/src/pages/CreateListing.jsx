2c4cf5c4bb14e14ae5b6722b04e69c5f64faaef2
architgupta225 <architgupta225@gmail.com>
2024-03-06 12:37:52 +0530

    complete create lising ui

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ export default function CreateListing() {
     return (
         <main className='p-3 max-w-4xl mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Create Listing</h1>
-            <form className='flex flex-col sm:flex-row'>
+            <form className='flex flex-col sm:flex-row gap-4'>
                 <div className='flex flex-col gap-4 flex-1'>
                     <input type='text'
                         className='border p-3 rounded-lg'
@@ -43,7 +43,7 @@ export default function CreateListing() {
                             <span>Offer</span>
                         </div>
                     </div>
-                    <div>
+                    <div className='flex flex-wrap gap-6'>
                         <div className='flex items-center gap-2'>
                             <input type='number' id='bedrooms' min='1' max='10'
                                 className='p-3 border border-gray-300 rounded-lg'
@@ -61,21 +61,38 @@ export default function CreateListing() {
                                 className='p-3 border border-gray-300 rounded-lg'
                                 required />
                             <div className='flex flex-col items-center'>
-                                 {/* 5:34:25 */}
                                 <p >Regular Price</p>
                                 <span className='text-xs'>($ / month)</span>
                             </div>
                         </div>
-                        <div className='flex items-center gap-2'>
-                            <input type='number' id='discountPrice' min='1' max='10'
-                                className='p-3 border border-gray-300 rounded-lg'
-                                required />
-                            <div className='flex flex-col items-center '>
-                                <p >Discounted Price</p>
-                                <span className='text-xs'>($ / month)</span>
+                        <div className='flex flex-wrap gap-6 '>
+                            <div className='flex items-center gap-2'>
+                                <input type='number' id='discountPrice' min='1' max='10'
+                                    className='p-3 border border-gray-300 rounded-lg'
+                                    required />
+                                <div className='flex flex-col items-center '>
+                                    <p >Discounted Price</p>
+                                    <span className='text-xs'>($ / month)</span>
+                                </div>
                             </div>
                         </div>
                     </div>
+
+                </div>
+                <div className='flex flex-col flex-1 gap-4'>
+                    <p className='font-semibold'>Images:
+                        <span className='font-normal text-gray-600 ml-2'>The first image will be the cover (max 6)
+                        </span>
+                    </p>
+                    <div className='flex gap-4'>
+                        <input className='p-3 border border-gray-300 rounded w-full' type='file' id='images' accept='images/*' multiple />
+                        <button className='p-3 text-green-700 border border-green-700 
+                        rounded uppercase hover:shadow-lg disabled:opacity-80'>
+                            Upload
+                        </button>
+                    </div>
+                    <button className='p-3 bg-slate-700 text-white rounded-lg
+                 uppercase hover:opactiy-95 disabled:opacity-80'>Create Listing</button>
                 </div>
             </form>
 
