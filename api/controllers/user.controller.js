036cc6f2fb58a1dc6a9d30829d1b79ba382e4cc1
architgupta225 <architgupta225@gmail.com>
2024-03-13 17:42:22 +0530

    complete listing

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -48,15 +48,15 @@ export const deleteUser = async (req, res) => {
   } catch (e) {}
 };
 
-export const getUserListing = async (req, res, next) => {
+export const getUserListings = async (req, res, next) => {
   if (req.user.id === req.params.id) {
     try {
-      const listing = await Listing.findById({ userRef: req.params.id });
-      res.status(200).json(listing);
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
     } catch (error) {
       next(error);
     }
   } else {
-    return next(errorHandler(401, "You can only view your own listings!"));
+    return next(errorHandler(401, 'You can only view your own listings!'));
   }
 };
