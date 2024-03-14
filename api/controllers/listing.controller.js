1ed3333ae476b9661e1273ba92ee77106cbf8e46
architgupta225 <architgupta225@gmail.com>
2024-03-14 17:19:38 +0530

    delete listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -20,8 +20,29 @@ export const deleteListing = async (req, res, next) => {
   }
 
   try {
-    await Listing.findByIdAndDelete(req.params.id); 
-    res.status(200).json('Listin has been deleted')
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findByIdAndDelete(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listing"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
   } catch (err) {
     next(err);
   }
